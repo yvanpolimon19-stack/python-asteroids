eb85926bf8eb835e4667a04b1d4c165876f4391f
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-28 18:09:09 +0400

    player can now move

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -28,8 +28,24 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt)
+            self.rotate(-dt)
             
         if keys[pygame.K_d]:
-            self.rotate(-dt)
+            self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+            
+        if keys[pygame.K_s]:
+            self.move(-dt)    
+
+    
+    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+
 
