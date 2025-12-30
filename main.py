5d54cc14b64ea48e3f2dd25d259f649b65c0e0ca
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 23:23:40 +0400

    k

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,13 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        drawable.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
-        dt = clock.tick(120) / 1000
+        dt = clock.tick(60) / 1000
         #fps = 1 /dt
         #print(fps)
 
